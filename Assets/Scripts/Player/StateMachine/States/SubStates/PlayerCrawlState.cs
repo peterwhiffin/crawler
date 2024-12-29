502e8e7d04007adc9a7b8d6040b492633d3bf3ac
peterwhiffin <jordanlahaie@gmail.com>
2024-12-29 00:26:17 -0600

    i broke something

--- Assets/Scripts/Player/StateMachine/States/SubStates/PlayerCrawlState.cs
+++ Assets/Scripts/Player/StateMachine/States/SubStates/PlayerCrawlState.cs
@@ -1,55 +1,16 @@
 using UnityEngine;
 
-public class PlayerCrawlState : PlayerGroundedState
+public class PlayerCrawlState : MonoBehaviour
 {
-    public PlayerCrawlState(StateMachine stateMachine, Player player, InputHandler inputHandler) : base(stateMachine, player, inputHandler)
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
+        
     }
 
-    public override void Enter()
+    // Update is called once per frame
+    void Update()
     {
-        base.Enter();
-    }
-
-    public override void Exit()
-    {
-        base.Exit();
-    }
-
-    public override void Update()
-    {
-        base.Update();
-        m_Player.Motor.CheckCurrentLeg();
-        m_Player.Motor.SetRestPosition();
-        m_Player.LookAtCursor();
-
-        if (m_InputHandler.StretchInput)
-        {
-            m_StateMachine.ChangeState(m_Player.StretchState);
-        }
-        else if(m_InputHandler.MoveInput == Vector2.zero)
-        {
-            m_StateMachine.ChangeState(m_Player.IdleState);
-        }
-    }
-
-    public override void FixedUpdate()
-    {
-        base.FixedUpdate();
-
-        Vector2 finalForce = Vector2.zero;
-        finalForce += m_Player.Motor.GetMoveForce(m_InputHandler.MoveInput);
-
-        if (m_Player.Motor.IsPlayerAtMaxDistance())
-        {
-            finalForce += m_Player.Motor.RestrainToRestPosition();
-        }
-
-        m_Player.Motor.MovePlayer(finalForce);
-    }
-
-    public override void LateUpdate()
-    {
-        base.LateUpdate();
+        
     }
 }
--- Assets/Scripts/Player/StateMachine/States/SubStates/PlayerIdleState.cs
+++ Assets/Scripts/Player/StateMachine/States/SubStates/PlayerIdleState.cs
@@ -1,63 +1,16 @@
 using UnityEngine;
 
-public class PlayerIdleState : PlayerGroundedState
+public class PlayerIdleState : MonoBehaviour
 {
-    public PlayerIdleState(StateMachine stateMachine, Player player, InputHandler inputHandler) : base(stateMachine, player, inputHandler)
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-    }
-
-    public override void Enter()
-    {
-        base.Enter();
-    }
-
-    public override void Exit()
-    {
-        base.Exit();
-    }
-
-    public override void Update()
-    {
-        base.Update();
-
-
-
-        if (m_Player.Motor.LegNeedsToMove())
-        {
-            m_Player.Motor.CheckCurrentLeg();
-        }
-        else if (m_Player.Motor.HasPlayerHitMoveThreshold())
-        {
-            m_Player.Motor.CheckCurrentLeg();
-        }
         
-
-        m_Player.Motor.SetRestPosition();
-        m_Player.LookAtCursor();
-
-
-        if (m_InputHandler.StretchInput)
-        {
-            m_StateMachine.ChangeState(m_Player.StretchState);
-        }
-        else if(m_InputHandler.MoveInput != Vector2.zero)
-        {
-            m_StateMachine.ChangeState(m_Player.CrawlState);
-        }
     }
 
-    public override void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        base.FixedUpdate();
-
-        Vector2 finalForce = m_Player.Motor.RestrainToRestPosition();
-        finalForce += m_Player.Motor.FloatOffTerrain();
-
-        m_Player.Motor.MovePlayer(finalForce);
+        
     }
-
-    public override void LateUpdate()
-    {
-        base.LateUpdate();
-    } 
 }
